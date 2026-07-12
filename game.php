a4d964322e5054bbb0ff8c81ba73afada1d52736
J. Freedom <seth.hedley@gmail.com>
2026-07-12 16:04:51 +0100

    update

--- game.php
+++ game.php
@@ -4,13 +4,13 @@ header("Content-Type: application/json");
 $universeId = $_GET['universeId'] ?? null;
 
 if (!$universeId) {
-    echo json_encode(["error" => "Missing universeId"]);
+    echo json_encode(["error" => "No universe ID"]);
     exit;
 }
 
-$url = "https://games.roblox.com/v1/games?universeIds=$universeId";
+$url = "https://games.roblox.com/v1/games?universeIds=" . $universeId;
 
-// simple fetch
 $response = file_get_contents($url);
 
 echo $response;
+?>
